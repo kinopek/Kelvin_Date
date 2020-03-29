e8078aa9753b94b7e610c28c974827c380372b1c
kinopek <34396864+kinopek@users.noreply.github.com>
2020-03-29 18:36:44 +0200

    gps coordinates

--- lib/main.dart
+++ lib/main.dart
@@ -2,23 +2,101 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:english_words/english_words.dart';
+
+
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+
+
+
+class MyApp  extends StatelessWidget  {
+
+
+
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
+      title: 'KelvinDate',//Nazwa Procesu
+      home: GeolocationExample(
+
+     /*home: Scaffold(
         appBar: AppBar(
-          title: Text('Welcome to Flutter'),
+          title: Text('KelvinDate'),//Gorny pasek
         ),
         body: Center(
-          child: Text('Hello World'),
-        ),
+          child: Text('KelvinDateeeeee'),//tresc
+
+      ),
+    */  ),
+    );
+  }
+
+}
+
+class GeolocationExampleState extends State
+{
+  Geolocator _geolocator;
+  Position _position;
+
+  void checkPermission() {
+    _geolocator.checkGeolocationPermissionStatus().then((status) { print('status: $status'); });
+    _geolocator.checkGeolocationPermissionStatus(locationPermission: GeolocationPermission.locationAlways).then((status) { print('always status: $status'); });
+    _geolocator.checkGeolocationPermissionStatus(locationPermission: GeolocationPermission.locationWhenInUse)..then((status) { print('whenInUse status: $status'); });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _geolocator = Geolocator();
+    LocationOptions locationOptions = LocationOptions(accuracy: LocationAccuracy.high, distanceFilter: 1);
+
+    checkPermission();
+    //    updateLocation();
+
+    StreamSubscription positionStream = _geolocator.getPositionStream(locationOptions).listen(
+            (Position position) {
+          _position = position;
+        });
+  }
+
+  void updateLocation() async {
+    try {
+      Position newPosition = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.high)
+          .timeout(new Duration(seconds: 5));
+
+      setState(() {
+        _position = newPosition;
+      });
+    } catch (e) {
+      print('Error: ${e.toString()}');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('KelvinDate'),
+      ),
+      body: Center(
+          child: Text(
+              'Latitude: ${_position != null ? _position.latitude.toString() : '0'},'
+                  ' Longitude: ${_position != null ? _position.longitude.toString() : '0'}'
+          )
       ),
     );
   }
+}
+
+class GeolocationExample extends StatefulWidget {
+  @override
+  GeolocationExampleState createState() => new GeolocationExampleState();
 }
