8d4a38a525b83255c6fdc4746094f7cd17c86cff
kinopek <34396864+kinopek@users.noreply.github.com>
2020-03-30 17:19:18 +0200

    error fix

---
+++ lib/geolocation.dart
@@ -0,0 +1,107 @@
+import 'dart:async';
+import 'dart:collection';
+//import 'dart:html';
+import 'dart:math' show cos, sqrt, asin;
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:english_words/english_words.dart';
+import 'package:geocoder/geocoder.dart';
+
+class GeolocationExampleState extends State
+{
+  Geolocator _geolocator;
+  Position _position; //your phone
+  Coordinates c = new Coordinates  (51.0, 17.0);//for testing
+  final _coordinates = new Coordinates  (51.1098966,17.0326828);//rynek for now
+  double _distance = 100.0;
+  Queue<double> _dist_archive= new Queue();
+
+  void checkPermission() {
+    _geolocator.checkGeolocationPermissionStatus().then((status) { print('status: $status'); });
+    _geolocator.checkGeolocationPermissionStatus(locationPermission: GeolocationPermission.locationAlways).then((status) { print('always status: $status'); });
+    _geolocator.checkGeolocationPermissionStatus(locationPermission: GeolocationPermission.locationWhenInUse)..then((status) { print('whenInUse status: $status'); });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _geolocator = Geolocator();
+    LocationOptions locationOptions = LocationOptions(accuracy: LocationAccuracy.high, distanceFilter: 1);
+
+    checkPermission();
+    //    updateLocation();
+
+    StreamSubscription positionStream = _geolocator.getPositionStream(locationOptions).listen(
+            (Position position)
+        {
+          _position = position;
+
+        });
+  }
+
+  void updateLocation() async {
+    try {
+
+      Position newPosition = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.high)
+          .timeout(new Duration(seconds: 5));
+      setState(() {
+        _position = newPosition;
+      });
+    } catch (e) {
+      print('Error: ${e.toString()}');
+    }
+  }
+
+  double updateDistance()
+  {
+    _distance=calculateDistance(_position.latitude, _position.longitude, _coordinates.latitude, _coordinates.longitude);
+    _dist_archive.add(_distance);
+    if(_dist_archive.length>10)
+    {
+      _dist_archive.removeFirst();
+    }
+    return _distance;
+  }
+
+  double calculateDistance(lat1, lon1, lat2, lon2)
+  {
+    if((lat1 == null)||(lon1 == null)||(lat2 == null)||(lon2 == null))
+    {
+      return -1.0;
+    }
+    var p = 0.017453292519943295;
+    var c = cos;
+    var a = 0.5 - c((lat2 - lat1) * p) / 2 +
+        c(lat1 * p) * c(lat2 * p) *
+            (1 - c((lon2 - lon1) * p)) / 2;
+    return 12742 * asin(sqrt(a));
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('KelvinDate'),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>
+        [
+            Text('Your Latitude: ${_position != null ? _position.latitude.toString() : 'processing'},' ),
+            Text('Your Longitude: ${_position != null ? _position.longitude.toString() : 'processing'}'),
+            Text(' Rynek Latitude: ${_coordinates != null ? _coordinates.latitude.toString() : 'processing'},'),
+            Text(' Rynek Longitude: ${_coordinates != null ? _coordinates.longitude.toString() : 'processing'}'),
+            Text(' Distance in kilometers: ${_position != null ? updateDistance() : 'processing'},' )
+        ]
+            )
+      ),
+    );
+  }
+}
+
+class GeolocationExample extends StatefulWidget {
+  @override
+  GeolocationExampleState createState() => new GeolocationExampleState();
+}
--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,8 @@ import 'dart:math' show cos, sqrt, asin;
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:english_words/english_words.dart';
-import 'package:geocoder/geocoder.dart';
+import 'geolocation.dart';
+
 
 void main() => runApp(MyApp());
 
@@ -26,113 +27,3 @@ class MyApp  extends StatelessWidget
 
 }
 
-class GeolocationExampleState extends State
-{
-  Geolocator _geolocator;
-  Position _position; //your phone
-  Coordinates c = new Coordinates  (51.0, 17.0);//for testing
-  final _coordinates = new Coordinates  (51.1098966,17.0326828);//rynek for now
-
-  void checkPermission() {
-    _geolocator.checkGeolocationPermissionStatus().then((status) { print('status: $status'); });
-    _geolocator.checkGeolocationPermissionStatus(locationPermission: GeolocationPermission.locationAlways).then((status) { print('always status: $status'); });
-    _geolocator.checkGeolocationPermissionStatus(locationPermission: GeolocationPermission.locationWhenInUse)..then((status) { print('whenInUse status: $status'); });
-  }
-
-  @override
-  void initState() {
-    super.initState();
-
-    _geolocator = Geolocator();
-    LocationOptions locationOptions = LocationOptions(accuracy: LocationAccuracy.high, distanceFilter: 1);
-
-    checkPermission();
-    //    updateLocation();
-
-    StreamSubscription positionStream = _geolocator.getPositionStream(locationOptions).listen(
-            (Position position)
-        {
-          _position = position;
-        });
-  }
-
-  void updateLocation() async {
-    try {
-
-
-      //final coordinates = new Coordinates(1.10, 45.50);
-      //var addresses = await Geocoder.local.findAddressesFromCoordinates(coordinates);
-     //_rynek = addresses.first.coordinates;
-
-      Position newPosition = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.high)
-          .timeout(new Duration(seconds: 5));
-
-
-      //_distanceInMeters = await calculateDist(_position as Coordinates, _coordinates);
-      //print( _distanceInMeters);
-
-      setState(() {
-        _position = newPosition;
-      });
-
-
-
-    } catch (e) {
-      print('Error: ${e.toString()}');
-    }
-  }
-/*
-  Future<double> calculateDist(Coordinates a, Coordinates b) async
-  {
-    double _d;
-    try {
-      double newDistance = await Geolocator().distanceBetween(a.latitude, a.longitude, b.latitude, b.longitude).timeout(new Duration(seconds: 5));
-
-      setState(() {
-        _d = newDistance;
-      });
-    } catch (e) {
-      print('Error: ${e.toString()}');
-    }
-    return _d;
-  }
-*/
-
-  double calculateDistance(lat1, lon1, lat2, lon2)
-  {
-    if((lat1 == null)||(lon1 == null)||(lat2 == null)||(lon2 == null))
-      {
-        return -1.0;
-      }
-    var p = 0.017453292519943295;
-    var c = cos;
-    var a = 0.5 - c((lat2 - lat1) * p) / 2 +
-        c(lat1 * p) * c(lat2 * p) *
-            (1 - c((lon2 - lon1) * p)) / 2;
-    return 12742 * asin(sqrt(a));
-  }
-
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('KelvinDate'),
-      ),
-      body: Center(
-          child: Text(
-              'Your Latitude: ${_position != null ? _position.latitude.toString() : '0'},'
-                  ' Your Longitude: ${_position != null ? _position.longitude.toString() : '0'}'
-                  ' Rynek Latitude: ${_coordinates != null ? _coordinates.latitude.toString() : '0'},'
-                  ' Rynek Longitude: ${_coordinates != null ? _coordinates.longitude.toString() : '0'}'
-                  ' Distance in kilometers: ${calculateDistance(_position.latitude, _position.longitude, _coordinates.latitude, _coordinates.longitude) != null && calculateDistance(_position.latitude, _position.longitude, _coordinates.latitude, _coordinates.longitude) >=0 ? calculateDistance(_position.latitude, _position.longitude, _coordinates.latitude, _coordinates.longitude) : '0'}'
-          )
-      ),
-    );
-  }
-}
-
-class GeolocationExample extends StatefulWidget {
-  @override
-  GeolocationExampleState createState() => new GeolocationExampleState();
-}
