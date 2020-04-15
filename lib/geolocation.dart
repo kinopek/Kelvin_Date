6d5ac09d1f9cae146e73d9e6a51896d3c9b3a356
kinopek <34396864+kinopek@users.noreply.github.com>
2020-04-15 18:29:50 +0200

    added thermometer bar

--- lib/geolocation.dart
+++ lib/geolocation.dart
@@ -9,22 +9,39 @@ import 'package:geolocator/geolocator.dart';
 import 'package:geocoder/geocoder.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'fire.dart';
+import 'package:flutter/widgets.dart';
 
-class GeolocationExampleState extends State
-{
+class GeolocationExampleState extends State {
+  //final double _width, _value, _totalValue;
+
+
   Geolocator _geolocator;
   Position _position; //your phone
-  Coordinates c = new Coordinates  (51.0, 17.0);//for testing
-  final _coordinates = new Coordinates  (51.1098966,17.0326828);//rynek for now, another user later
-  double _distance = 100.0;
-  Queue<double> _dist_archive= new Queue();
+  double  _startingDistance = 100.0, _distance = 100.0;
+  Coordinates c = new Coordinates(51.0, 17.0); //for testing
+  final _coordinates = new Coordinates(
+  //    51.1098966, 17.0326828); //rynek for now, another user later
+      51.043, 17.079);//karels house
+
+  Queue<double> _dist_archive = new Queue();
   static Fire f = new Fire();
-  Coordinates c2 = f.getCoordinates( );//to get rynek from database
+  Coordinates c2 = f.getCoordinates(); //to get rynek from database
 
   void checkPermission() {
-    _geolocator.checkGeolocationPermissionStatus().then((status) { print('status: $status'); });
-    _geolocator.checkGeolocationPermissionStatus(locationPermission: GeolocationPermission.locationAlways).then((status) { print('always status: $status'); });
-    _geolocator.checkGeolocationPermissionStatus(locationPermission: GeolocationPermission.locationWhenInUse)..then((status) { print('whenInUse status: $status'); });
+    _geolocator.checkGeolocationPermissionStatus().then((status) {
+      print('status: $status');
+    });
+    _geolocator
+        .checkGeolocationPermissionStatus(
+            locationPermission: GeolocationPermission.locationAlways)
+        .then((status) {
+      print('always status: $status');
+    });
+    _geolocator.checkGeolocationPermissionStatus(
+        locationPermission: GeolocationPermission.locationWhenInUse)
+      ..then((status) {
+        print('whenInUse status: $status');
+      });
   }
 
   @override
@@ -32,23 +49,24 @@ class GeolocationExampleState extends State
     super.initState();
 
     _geolocator = Geolocator();
-    LocationOptions locationOptions = LocationOptions(accuracy: LocationAccuracy.high, distanceFilter: 1);
+    LocationOptions locationOptions =
+        LocationOptions(accuracy: LocationAccuracy.high, distanceFilter: 1);
 
     checkPermission();
     updateLocation();
 
-    StreamSubscription positionStream = _geolocator.getPositionStream(locationOptions).listen(
-            (Position position)
-        {
-          _position = position;
+    StreamSubscription positionStream = _geolocator
+        .getPositionStream(locationOptions)
+        .listen((Position position) {
+      _position = position;
+    });
 
-        });
   }
 
   void updateLocation() async {
     try {
-
-      Position newPosition = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.high)
+      Position newPosition = await Geolocator()
+          .getCurrentPosition(desiredAccuracy: LocationAccuracy.high)
           .timeout(new Duration(seconds: 5));
       setState(() {
         _position = newPosition;
@@ -58,35 +76,34 @@ class GeolocationExampleState extends State
     }
   }
 
-  double updateDistance()
-  {
-    setState(()
-    {
-      _distance=calculateDistance(_position.latitude, _position.longitude, _coordinates.latitude, _coordinates.longitude);
+  double updateDistance() {
+    setState(() {
+      _distance = calculateDistance(_position.latitude, _position.longitude,
+          _coordinates.latitude, _coordinates.longitude);
     });
 
     _dist_archive.add(_distance);
-    if(_dist_archive.length>10)
-    {
+    if (_dist_archive.length > 10) {
       _dist_archive.removeFirst();
     }
+    else if(_dist_archive.length == 1) {
+      _startingDistance = _distance;
+    }
+
     return _distance;
   }
 
-  double calculateDistance(lat1, lon1, lat2, lon2)
-  {
-    if((lat1 == null)||(lon1 == null)||(lat2 == null)||(lon2 == null))
-    {
+  double calculateDistance(lat1, lon1, lat2, lon2) {
+    if ((lat1 == null) || (lon1 == null) || (lat2 == null) || (lon2 == null)) {
       return -1.0;
     }
     var p = 0.017453292519943295;
     var c = cos;
-    var a = 0.5 - c((lat2 - lat1) * p) / 2 +
-        c(lat1 * p) * c(lat2 * p) *
-            (1 - c((lon2 - lon1) * p)) / 2;
+    var a = 0.5 -
+        c((lat2 - lat1) * p) / 2 +
+        c(lat1 * p) * c(lat2 * p) * (1 - c((lon2 - lon1) * p)) / 2;
     return 12742 * asin(sqrt(a));
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -96,36 +113,55 @@ class GeolocationExampleState extends State
         backgroundColor: Colors.red,
       ),
       body: Center(
-        child: Column(
-          children: <Widget>
-        [
-            Text('Your Latitude: ${_position != null ? _position.latitude.toString() : 'processing'},',
-          style: TextStyle(
-             // color: Colors.red,
-              fontSize: 20
-          )
-      ),
-            Text('Your Longitude: ${_position != null ? _position.longitude.toString() : 'processing'}',
-                style: TextStyle(
+        child: Column(children: <Widget>[
+          Text(
+              'Your Latitude: ${_position != null ? _position.latitude.toString() : 'processing'},',
+              style: TextStyle(
                   // color: Colors.red,
-                    fontSize: 20
-                )
-            ),
-            Text(' Rynek Latitude: ${_coordinates != null ? _coordinates.latitude.toString() : 'processing'},'),
-            Text(' Rynek Longitude: ${_coordinates != null ? _coordinates.longitude.toString() : 'processing'}'),
-            Text(' Distance in kilometers: ${_position != null ? updateDistance() : 'processing'},' ,
-                style: TextStyle(
+                  fontSize: 20)),
+          Text(
+              'Your Longitude: ${_position != null ? _position.longitude.toString() : 'processing'}',
+              style: TextStyle(
                   // color: Colors.red,
-                    fontSize: 20
-                )
+                  fontSize: 20)),
+          Text(
+              ' Rynek Latitude: ${_coordinates != null ? _coordinates.latitude.toString() : 'processing'},'),
+          Text(
+              ' Rynek Longitude: ${_coordinates != null ? _coordinates.longitude.toString() : 'processing'}'),
+          Text(
+              ' Distance in kilometers: ${_position != null ? updateDistance() : 'processing'},',
+              style: TextStyle(
+                  // color: Colors.red,
+                  fontSize: 20)),
+          Row(children: <Widget>[
+            Expanded(
+                child: Column(children: <Widget>[
+              Icon(Icons.place),
+              Text('Distance', textAlign: TextAlign.center)
+            ])),
+            Expanded(
+              child:
+              Thermometer(100.0, _distance, _startingDistance),
+
             ),
-            RaisedButton(child: Text('Save to Database'), onPressed: () {f.createRecord(_position.latitude, _position.longitude);},),
-            RaisedButton(child: Text('Logout'), onPressed: () {
-              FirebaseAuth.instance.signOut();
-              Navigator.pushReplacementNamed(context, "/login");
-            },)
-        ]
-            )
+
+          ]),
+          Row(children: <Widget>[
+            RaisedButton(
+              child: Text('Save to Database'),
+              onPressed: () {
+                f.createRecord(_position.latitude, _position.longitude);
+              },
+            ),
+            RaisedButton(
+              child: Text('Logout'),
+              onPressed: () {
+                FirebaseAuth.instance.signOut();
+                Navigator.pushReplacementNamed(context, "/login");
+              },
+            ),
+          ]),
+        ]),
       ),
     );
   }
@@ -134,4 +170,52 @@ class GeolocationExampleState extends State
 class GeolocationExample extends StatefulWidget {
   @override
   GeolocationExampleState createState() => new GeolocationExampleState();
+}
+
+class Thermometer extends StatelessWidget
+{
+  final double thermoHeight, value, totalValue;
+
+  Thermometer(this.thermoHeight, this.value, this.totalValue);
+
+  @override
+  Widget build(BuildContext context)
+  {
+    double ratio= value / totalValue;
+
+return Stack(children: <Widget>
+[
+  Container
+    (
+    width: 10,
+    height: thermoHeight,
+    decoration: BoxDecoration
+      (
+        color: Colors.lightBlueAccent,
+        borderRadius: BorderRadius.circular(5)
+    ),
+    child: Align
+      (
+        alignment: Alignment(0.0, 1.0),
+        child: Material
+          (
+          borderRadius: BorderRadius.circular(5),
+          child: AnimatedContainer(
+            width: 10,
+            height: thermoHeight * (1-ratio) ,
+            duration: Duration(milliseconds: 500),
+            decoration: BoxDecoration(
+                color: (ratio < 0.2) ? Colors.deepOrangeAccent : (ratio < 0.7) ? Colors.redAccent : Colors.red,
+                borderRadius: BorderRadius.circular(5)),
+          ),
+        ),
+
+
+    ),
+
+  ),
+]
+);
+
+  }
 }
